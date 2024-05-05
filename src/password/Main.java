b777f3ff5f70f0c5a5c04c4eddf1bdb3991adea9
birdex66 <148392029+birdex66@users.noreply.github.com>
2024-05-05 13:44:30 -0700

    Add files via upload

---
+++ src/password/Main.java
@@ -0,0 +1,385 @@
+// Import statements
+package password;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+// Main class
+public class Main {
+    // Scanner object for user input
+    private static Scanner scan = new Scanner(System.in);
+    // Object for setup credentials
+    private static setup naurnaur = new setup();
+    // Constants for file names
+    private static final String DATA_FILE = ".dat";
+    private static final String ENCRYPTED_FILE = "encrypted";
+    // Flag to indicate if it's a master login
+    private static boolean master;
+    // String to check username and password
+    private static String check = "";
+    // Default input username and password
+    private static String inputName = "username";
+    private static String inputPassword = "password";
+
+    // Main method
+    public static void main(String[] args) throws Exception{
+        // Object for handling user account operations
+        passCreator account = new passCreator();
+        boolean access = false;
+        
+        // Loop for account creation or login
+        while(!access) {
+            System.out.printf("Press Y for a new account\nPress N to log in\nPress T to launch test cases\n");
+            String choice = scan.next();
+            if(choice.equals("Y")) {
+                createAccount();
+            }else if(choice.equals("N")) {
+                access = login();
+            }else if(choice.equals("T")) {
+            	 try {
+                     PassCreatorInterface passCreator = (PassCreatorInterface) new passCreator("temp.dat");
+                     
+                     passCreator.addAccount("user1", "password1");
+                     passCreator.addAccount("user2", "password2");
+                     
+                     System.out.printf("Sample data:\n");
+                     
+                     passCreator.printAllAccounts();
+                     
+                     passNew foundAccount = passCreator.getAccountByName("user1", "password1");
+                     if (foundAccount != null) {
+                         System.out.println("Found Account: " + foundAccount.getUsername());
+                     } else {
+                         System.out.println("Account not found.");
+                     }
+                     
+                     passCreator.remove(foundAccount);
+                     
+                     passCreator.printAllAccounts();
+                     clearFile("temp.dat");
+                     System.exit(0);
+                 } catch (IOException e) {
+                     e.printStackTrace();
+                 }
+            }else {
+                System.out.println("Invalid choice. Please try again.");
+            }
+        }
+        
+        // Main menu
+        if(access) {
+            String choice= "-1";
+            System.out.printf("\nopening user files..\n");
+            String inName = "";
+            String outName = "";
+            if(master) {
+                inName = account.getProtectedFileName();
+                outName = account.getFileName();
+            }else {
+                inName = ENCRYPTED_FILE + check + DATA_FILE;
+                outName = check + DATA_FILE;
+            }
+            decryptFile(inName, outName, inputPassword);
+            clearFile(inName);
+            account = openDataFile(outName);
+            System.out.print("files opened successfully.\n");
+            
+            // Loop for user operations
+            while(!choice.equals("5")) {
+                displayMenu();
+                choice = scan.next();
+                
+                // View passwords
+                if(choice.equals("1")){
+                    account.printAllAccounts();
+                    System.out.printf("\nAccount count: %d\n\n",account.getAccountCount() );
+                // Add new password
+                }else if(choice.equals("2")) {
+                    System.out.print("Enter your new username: ");
+                    String name = scan.next();
+                    System.out.print("Enter your new password: ");
+                    String password = scan.next();
+                    account.addAccount(name, password);
+                // Edit password
+                }else if(choice.equals("3")) {
+                    System.out.print("Enter the username to edit: ");
+                    String user = scan.next();
+                    System.out.print("Enter the password to edit: ");
+                    String password = scan.next();
+                    passNew reference = account.getAccountByName(user, password);
+                    if(reference != null) {
+                        editPassword(outName, "temp.dat" , password, user, reference);
+                    }
+                // Remove password
+                }else if(choice.equals("4")) {
+                    System.out.print("Enter the username to remove: ");
+                    String user = scan.next();
+                    System.out.print("Enter the password to remove: ");
+                    String password = scan.next();
+                    passNew reference = account.getAccountByName(user, password);
+                    if(reference != null) {
+                        account.remove(reference);
+                        removePassword(outName, "temp.dat" , password, user);
+                    }
+                // Exit
+                }else if(choice.equals("5")) {
+                    encryptFile(outName, inName, inputPassword);
+                    clearFile(outName);
+                    System.out.printf("\nExiting...\n");
+                }else{
+                	System.out.println("Invalid Choice. Exiting...");
+                    System.exit(1);
+                }
+            }
+        }
+    }
+
+    // Method to edit password
+    private static void editPassword(String outFile, String inFile, String pass, String user, passNew reference) throws FileNotFoundException {
+        try (Scanner in = new Scanner(new File(outFile));
+             PrintWriter out = new PrintWriter(inFile)) {
+            while(in.hasNextLine()) {
+                String line = in.nextLine();
+                if(line.equals(user + " " +  pass)) {
+                    System.out.printf("\nEnter your new username: ");
+                    String username = scan.next();
+                    System.out.print("Enter your new password: ");
+                    String password = scan.next();
+                    line = username + " " + password;
+                    
+                    reference.setUsername(username);
+                    reference.setPassword(password);
+                }
+                out.println(line);
+            }
+            
+            in.close();
+            out.close();
+            
+            
+            clearFile(outFile);
+            copyFile(inFile,outFile);
+            clearFile(inFile);
+        }
+        catch(Exception error) {
+            System.out.println("Error while editing password." + error.getMessage());
+        }
+    }
+    
+    // Method to remove password
+    private static void removePassword(String outFile, String inFile, String pass, String user) throws FileNotFoundException {
+        try (Scanner in = new Scanner(new File(outFile));
+             PrintWriter out = new PrintWriter(inFile)) {
+            while(in.hasNextLine()) {
+                String line = in.nextLine();
+                if(line.equals(user + " " +  pass)) {
+                    out.println();
+                }else {
+                    out.println(line);
+                }
+            }
+            
+            in.close();
+            out.close();
+            
+            
+            clearFile(outFile);
+            copyFile(inFile,outFile);
+            clearFile(inFile);
+        }
+        catch(Exception error) {
+            System.out.println("Error while removing password." + error.getMessage());
+        }
+    }
+    
+    // Method to copy file
+    private static void copyFile(String inFile, String outFile)throws IOException{
+        try (Scanner in = new Scanner(new File(inFile));
+             PrintWriter out = new PrintWriter(outFile)) {
+                while (in.hasNextLine()) {
+                    out.println(in.nextLine());
+                }
+            }catch (IOException e) {
+                System.out.println("Error while copying file: " + e.getMessage());
+            }
+    }
+    
+    // Method to create new account
+    private static void createAccount() throws Exception {
+        System.out.print("Enter your username: ");
+        String name = scan.next();
+        
+        System.out.print("Enter your password\n(must be 16 or less characters):\n ");
+        String password = scan.next();
+        
+        String encryptedFileName = encryptFileName(password,name);
+        
+        if(fileExists(encryptedFileName)) {
+            System.out.println("Username taken, please use a new name");
+            return;
+        }else if (password.length() > 16){
+        	while(password.length() > 16){
+        		System.out.print("Password length is longer than 16 characters.\nEnter a new password:\n");
+        		password = scan.next();
+        	}
+        }
+            naurnaur.setUsername(name);
+            naurnaur.setPassword(password);
+            createUserFile(encryptFileName(password,name));
+            createUserFile(ENCRYPTED_FILE + encryptFileName(password,name));
+    }
+    
+    // Method for user login
+    private static boolean login() throws Exception{
+        System.out.printf("\nEnter your username: ");
+        inputName = scan.next();
+        
+        System.out.print("Enter your password: ");
+        inputPassword = scan.next();
+        check = encryptFileName(inputPassword,inputName);
+        master = inputName.equals("0") && inputPassword.equals("0");
+
+        if(fileExists(check) || master) {
+            naurnaur.setUsername(inputName);
+            naurnaur.setPassword(inputPassword);
+            return true;
+        }else {
+            System.out.printf("\nUsername or password is incorrect.\n\n");
+            return false;
+        }
+    }
+        
+    // Method to display main menu
+    private static void displayMenu() {
+        System.out.println();
+        System.out.println("Welcome, " + naurnaur.getUsername() + ".");
+        System.out.printf ("\n=======Options=======\n");
+        System.out.println("1:  View Passwords  ");
+        System.out.println("2:   Add Password   ");
+        System.out.println("3:  Edit Password  ");
+        System.out.println("4:  Remove Password  ");
+        System.out.println("5:   Exit Program   ");
+    }
+    
+    // Method to open data file
+    private static passCreator openDataFile(String fileName) throws IOException {
+        return new passCreator(fileName);
+      }
+    
+    // Method to clear file
+    private static void clearFile(String fileName){
+        try {
+        FileWriter out = new FileWriter(fileName, false);
+        out.close();
+        }catch(IOException error){
+            System.out.println("Error while clearing the file: " + error.getMessage());
+        }
+    }
+    
+    // Method to encrypt file
+    private static void encryptFile(String inFile, String outFile, String key)throws FileNotFoundException {
+        String add = "0000000000000000";
+        if(key.length() < 16) {
+            key += add.substring(0,16 - key.length());
+        }
+         try (Scanner in = new Scanner(new File(inFile));
+                 PrintWriter out = new PrintWriter(outFile)) {
+             
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), "AES"),new IvParameterSpec("aaaabbbbccccdddd".getBytes("ASCII")));
+            
+            while(in.hasNextLine()) {
+                String to = in.nextLine();
+                byte[] gibb = cipher.doFinal(to.getBytes(StandardCharsets.UTF_8));
+                String encryptedBase64 = Base64.getEncoder().encodeToString(gibb);
+                out.println(encryptedBase64);
+                }
+            in.close();
+            out.close();
+         }catch(Exception error) {
+            System.out.println("Error while encrypting file." + error.getMessage());
+        }
+    }
+    
+    // Method to decrypt file
+    private static void decryptFile(String inFile, String outFile, String key)throws FileNotFoundException {
+        String add = "0000000000000000";
+        if(key.length() < 16) {
+            key += add.substring(0,16 - key.length());
+        }
+        
+         try (Scanner in = new Scanner(new File(inFile));
+                 PrintWriter out = new PrintWriter(outFile)) {
+             
+             
+             Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+             cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), "AES"), new IvParameterSpec("aaaabbbbccccdddd".getBytes("ASCII")));
+                 
+        while(in.hasNextLine()) {    
+            String to = in.nextLine();
+            byte[] bbig = Base64.getDecoder().decode(to);
+            byte[] decrypted = cipher.doFinal(bbig);
+            String decryptedBase64 = new String(decrypted, StandardCharsets.UTF_8);
+            out.println(decryptedBase64);
+            
+        }
+        //System.out.println("decrypted");
+        in.close();
+        out.close();
+        }catch(Exception error) {
+            System.out.println("Error while decrypting file.Exiting...\n" + error.getMessage());
+            error.printStackTrace();
+            System.exit(2);
+        }
+    }
+     
+    // Method to create user file
+    static void createUserFile(String name)throws IOException{
+         boolean works;
+        File f = new File(name + DATA_FILE);
+            works = f.createNewFile();
+        if (works) {
+                System.out.println("File created successfully.");
+            } else {
+                System.out.println("File creation failed.");
+            }
+     }
+     
+    // Method to check if file exists
+    public static boolean fileExists(String fileName){
+         File f = new File(fileName + DATA_FILE);
+         return f.exists();
+     }
+     
+    // Method to check if file is empty
+    public static boolean fileEmpty(String fileName) {
+         File f = new File(fileName + DATA_FILE);
+         return f.length() == 0;
+     }
+     
+    // Method to encrypt file name
+     public static String encryptFileName(String key, String text) throws Exception {
+         String add = "0000000000000000";
+         if(key.length() < 16) {
+             key += add.substring(0,16 - key.length());
+         }else if(key.length() > 16) {
+        	 return null;
+         }
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), "AES"), new IvParameterSpec("aaaabbbbccccdddd".getBytes("ASCII")));
+            byte[] encrypted = cipher.doFinal(text.getBytes(StandardCharsets.UTF_8));
+            String encryptedBase64 = Base64.getEncoder().encodeToString(encrypted);
+            return encryptedBase64.replace("/","_");
+        }
+
+      /*  public static byte[] decrypt(String key, byte[] encryptedBytes) throws Exception {
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), "AES"), new IvParameterSpec("aaaabbbbccccdddd".getBytes("ASCII")));
+            byte[] decrypted = cipher.doFinal(encryptedBytes);
+            return decrypted;
+        }*/
+}
---
+++ src/password/passCreator.java
@@ -0,0 +1,112 @@
+// Import statements
+package password;
+
+import java.io.*;
+import java.util.*;
+
+// passCreator class
+class passCreator implements PassCreatorInterface{
+    // ArrayList to store passNew objects
+    private ArrayList<passNew> accountList;
+    // File name for storing passwords
+    private String fileName = "passwords.dat";
+    // File name for storing encrypted passwords
+    private String protectedFileName = "encryptedPasswords.dat";
+
+    // Constructor to initialize accountList
+    public passCreator() {
+        accountList = new ArrayList<passNew>();
+    }
+    
+    // Constructor to initialize accountList from a file
+    public passCreator(String fileName) throws IOException{
+        this.fileName = fileName;
+        accountList = new ArrayList<passNew>();
+        FileInputStream myFile = new FileInputStream(fileName);
+        Scanner myFileReader = new Scanner (myFile);
+        
+        // Reading usernames and passwords from the file
+        while (myFileReader.hasNext()){
+            String username = myFileReader.next();
+            String password = myFileReader.next();
+            addFileAccount(username, password);
+         }
+         myFileReader.close();
+    }
+    
+    // Method to add a passNew object to accountList
+    public void addFileAccount(String username, String password){
+        passNew newAccount = new passNew(username, password);
+        accountList.add(newAccount);
+    }
+    
+    // Method to add a new account with username and password
+    public void addAccount(String username, String password){
+        passNew newAccount = new passNew(username, password);
+        accountList.add(newAccount);
+        addToFile(newAccount);
+    }
+    
+    // Method to add account information to the file
+    private void addToFile(passNew account){
+        try {
+            FileWriter filewriter = new FileWriter(this.fileName,true);
+            PrintWriter printwriter = new PrintWriter(filewriter);
+            printwriter.println(account.getUsername() + " " + account.getPassword());
+            printwriter.close();
+            filewriter.close();    
+        } catch(IOException error){
+            System.out.println("Error saving account to file: " + error.getMessage());
+        }
+    }
+    
+    // Getter method for fileName
+    public  String getFileName() {
+        return this.fileName;
+    }
+    
+    // Getter method for protectedFileName
+    public String getProtectedFileName() {
+        return this.protectedFileName;
+    }
+    
+    // Method to get the number of accounts
+    public int getAccountCount() {
+        return accountList.size();
+    }
+    
+    // Method to remove an account from accountList
+    public void remove(passNew reference) {
+        accountList.remove(reference);
+    }
+    
+    // Method to get passNew object by username and password
+    public passNew getAccountByName(String username, String password) {
+        for(int i=0; i<accountList.size(); ++i) {
+            passNew reference = accountList.get(i);
+            String userReference = reference.getUsername();
+            String passReference = reference.getPassword();
+            int settingReference = userReference.indexOf(username);
+            int setReference = passReference.indexOf(password);
+            if(settingReference != -1 && setReference != -1) {
+                return reference;
+            }
+        }
+        System.out.printf("\nAccount does not exist\n");
+        return null;            
+    }
+    
+    // Method to print all accounts
+    public void printAllAccounts(){
+        if(accountList.size() == 0) {
+            System.out.printf("\nNo accounts have been added\n");
+            return;
+        }
+        System.out.printf("\n User   Pass\n");
+        for(int i=0; i<accountList.size(); ++i) {
+            passNew reference = accountList.get(i);    
+            reference.printInfo();
+            System.out.println("---------------");
+        }
+    }
+}
